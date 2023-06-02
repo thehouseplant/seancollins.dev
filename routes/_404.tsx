314ca0a23347f28641c8421f71ed19c9e4abaf07
Sean Collins <sean.collins@outlook.com>
2023-06-02 16:35:54 -0700

    Update error logic to catch all server codes

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,10 +1,15 @@
+import { Header } from "../components/Header.tsx";
 import { Footer } from "../components/Footer.tsx";
 
-export default function NotFoundPage() {
+export function ServerCodePage(props: { serverCode: number, codeDescription: string }) {
   return (
     <>
+      <Header />
       <div class="bg-white w-full py-2 px-10 flex flex-col dark:(bg-gray-900 text-gray-300)">
-        <h1>404 Not Found</h1>
+        <h1>{props.serverCode}</h1>
+        <p>
+          {props.codeDescription}
+        </p>
         <p>
           <a href="/">Return Home</a>
         </p>
@@ -12,4 +17,13 @@ export default function NotFoundPage() {
       <Footer />
     </>
   )
+}
+
+export default function PageNotFound() {
+  return (
+    <ServerCodePage
+      serverCode={404}
+      codeDescription={"We couldn't find the page you're looking for."}
+    />
+  );
 }
