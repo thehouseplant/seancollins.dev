dba123126ad0a886e65ef75f85531b3bb6cc66f1
Sean Collins <sean.collins@outlook.com>
2023-06-02 16:46:58 -0700

    Integrate post preview component

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,17 +1,31 @@
-import { Head } from "$fresh/runtime.ts";
-import { Container } from "../components/Container.tsx";
 
-export default function HomePage() {
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { listPosts, Post } from "../utils/posts.ts";
+import { Container } from "../components/Container.tsx";
+import { PostPreview } from "../components/PostPreview.tsx";
+
+interface Data {
+  posts: Post[];
+}
+
+export const handler: Handlers<Data> = {
+  async GET(_req, ctx) {
+    const posts = await listPosts();
+    return ctx.render({ ...ctx.state, posts });
+  },
+};
+
+export default function Home(props: PageProps<Data>) {
+  const { posts } = props.data;
   return (
     <>
-      <Head>
-        <title>Sean Collins</title>
-      </Head>
-      <main>
-        <Container>
-          <h1>Home</h1>
-        </Container>
-      </main>
+     <main>
+      <Container>
+        <ul class="mt-16">
+          {posts.map((post) => <PostPreview post={post} />)}
+        </ul>
+      </Container>
+     </main>
     </>
   );
 }
